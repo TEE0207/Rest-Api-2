f30bae558e9f4e24fe5dd00867e9c90ce5108b4f
Taofeek Lawal <taofeeklwl@gmail.com>
2025-06-18 17:23:27 +0100

    path updated

--- src/OurListLoader.jsx
+++ src/OurListLoader.jsx
@@ -1,13 +1,10 @@
 
 
 export const countriesLoader = async () => {
-    const response = await fetch('https://restcountries.com/v3.1/all');
+    const response = await fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,subregion,flags,population,languages,area,cca2,cca3');
     if (!response.ok) {
       // throw new Error(`HTTP error! status: ${response.status}`);
     }
     return await response.json();
   }; 
-  
-
-
 
